d255683e7a353582e9d5daf6ffcd09a822aa671a
NatanRot <nathan.sentsov@gmail.com>
2018-12-26 20:41:08 +0200

    Config before GoogleCloud

--- middleaware/validate.js
+++ middleaware/validate.js
@@ -1,18 +1,22 @@
 const { check, body, query, validationResult} = require('express-validator/check');
 
 module.exports.id_validation = id_validation = [
-    check("id")
+    query("id")
     .exists()
     .not().isEmpty()
     .trim()
     .isAlphanumeric()
     .isMongoId() , function(req, res, next) {
         const errors = validationResult(req);
-        if(errors.isEmpty()) {
+        if(errors) {
             return res.status(500).json({error: errors.array()});
         }
 
         next();
     }
+];
+
+module.exports.search_validation = search_validation = [
+    
 ];
 
