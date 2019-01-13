ceba593ed051af1181cf6df84e3e42fe28c00ae6
Vladi Rachlin <vladi@colu.com>
2019-01-13 11:45:10 +0200

    cors

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-var express             = require('express');
-var morgan              = require('morgan');
-var mongoose            = require('mongoose');
-var synagogueRouter     = require('./routes/synagogue');
+var express = require('express');
+var morgan = require('morgan');
+var mongoose = require('mongoose');
+var synagogueRouter = require('./routes/synagogue');
+var cors = require('cors');
 
 var app = express();
 
@@ -12,18 +13,18 @@ app.use(morgan('dev'));
 // We won't use bodyParser, Express4.X^
 app.use(express.urlencoded());
 app.use(express.json());
-
+app.use(cors({ origin: '*' }))
 
 // Mount the routers
-app.use('/synagogue' , synagogueRouter);
+app.use('/synagogue', synagogueRouter);
 
 // Server init
 app.listen(8080, function () {
-    // Assuming a local mongodb instance with the db
-    // jewishTest is a test db
-    mongoose.connect('mongodb://startach:gG123456@ds235022.mlab.com:35022/jewish_world' , { useNewUrlParser: true })
-        .then(() => console.log('mongodb: connection succesful'))
-        .catch((err) => console.error(err));
-    console.log('express: listening on 3000');
+  // Assuming a local mongodb instance with the db
+  // jewishTest is a test db
+  mongoose.connect('mongodb://startach:gG123456@ds235022.mlab.com:35022/jewish_world', { useNewUrlParser: true })
+    .then(() => console.log('mongodb: connection succesful'))
+    .catch((err) => console.error(err));
+  console.log('express: listening on 3000');
 });
 
