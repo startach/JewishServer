25cdee495318acbc15069782284a548842ef9a71
Tuan Anh Le <anhletuanqt@gmail.com>
2020-01-20 14:34:28 +0700

    aws: write api preSignURL

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,21 @@
 import * as bodyParser from 'body-parser';
 import * as express from 'express';
-import { SynagoguesRouter } from "./routes/SynagoguesRouter";
-import { LessonsRouter } from "./routes/LessonsRouter";
+import { SynagoguesRouter } from './routes/SynagoguesRouter';
+import { LessonsRouter } from './routes/LessonsRouter';
 import { AuthRouter } from './routes/AuthRouter';
 import { HomeRouter } from './routes/HomeRouter';
 import { SplashRouter } from './routes/SplashRouter';
 import { UsersRouter } from './routes/UsersRouter';
 import { SearchRouter } from './routes/SearchRouter';
+import { AWSRouter } from './routes/AWSRouter';
 import * as passport from 'passport';
-import "reflect-metadata";
-import * as swagger from "swagger-express-ts";
+import 'reflect-metadata';
+import * as swagger from 'swagger-express-ts';
 import * as swaggerUi from 'swagger-ui-express';
 const swaggerDocument = require('../swagger.json');
 import { updateMinyan } from './update_minyan';
 import * as i18n from 'i18n';
-import * as path from "path";
+import * as path from 'path';
 
 const FacebookTokenStrategy = require('passport-facebook-token');
 const GoogleTokenStrategy = require('passport-google-id-token'); //GoogleTokenStrategy = require('passport-google-token').Strategy;
@@ -34,8 +35,8 @@ const app = express();
 
 //config for internationalization
 i18n.configure({
-  locales:['en', 'iw'],
-  directory: path.resolve(process.cwd(), 'src/locales')
+  locales: ['en', 'iw'],
+  directory: path.resolve(process.cwd(), 'src/locales'),
 });
 
 app.set('i18n', i18n);
@@ -63,47 +64,56 @@ app.use(bodyParser.json());
 //   }
 // ));
 
-passport.use(new FacebookTokenStrategy({
-    clientID: FACEBOOK_APP_ID,
-    clientSecret: FACEBOOK_APP_SECRET,
-    profileFields: ['id', 'email', 'name', 'gender', 'picture.type(large)']
-  },
-  function(accessToken, refreshToken, user, done) {
-    done(null, user);
-  }
-));
+passport.use(
+  new FacebookTokenStrategy(
+    {
+      clientID: FACEBOOK_APP_ID,
+      clientSecret: FACEBOOK_APP_SECRET,
+      profileFields: ['id', 'email', 'name', 'gender', 'picture.type(large)'],
+    },
+    function(accessToken, refreshToken, user, done) {
+      done(null, user);
+    },
+  ),
+);
 
-passport.use(new GoogleTokenStrategy({
-  clientID: GOOGLE_CLIENT_ID,
-  //clientSecret: GOOGLE_CLIENT_SECRET
-},
-function(parsedToken, googleId, done) {
-  done(null, parsedToken.payload);
-}
-));
+passport.use(
+  new GoogleTokenStrategy(
+    {
+      clientID: GOOGLE_CLIENT_ID,
+      //clientSecret: GOOGLE_CLIENT_SECRET
+    },
+    function(parsedToken, googleId, done) {
+      done(null, parsedToken.payload);
+    },
+  ),
+);
 
 app.use((err, req, res, next) => {
   //catch incorrect json err
   // @ts-ignore
   if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
-      console.error(err);
-      return res.sendStatus(400);
+    console.error(err);
+    return res.sendStatus(400);
   }
 
   next();
 });
 
-passport.use(new JwtStrategy({
-  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-  secretOrKey   : JWT_SECRET,
-  passReqToCallback: true
-},
-function (req, jwtPayload, done) {
-  req.id = jwtPayload.id;
-  req.userEmail = jwtPayload.email;
-  done(null, jwtPayload);
-}
-));
+passport.use(
+  new JwtStrategy(
+    {
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: JWT_SECRET,
+      passReqToCallback: true,
+    },
+    function(req, jwtPayload, done) {
+      req.id = jwtPayload.id;
+      req.userEmail = jwtPayload.email;
+      done(null, jwtPayload);
+    },
+  ),
+);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', new SplashRouter().router);
@@ -113,9 +123,12 @@ app.use('/synagogue', new SynagoguesRouter().router);
 app.use('/lesson', new LessonsRouter().router);
 app.use('/users', new UsersRouter().router);
 app.use('/search', new SearchRouter().router);
+app.use('/aws', new AWSRouter().router);
 
-setTimeout(() => { updateMinyan(); }, 3000)
+setTimeout(() => {
+  updateMinyan();
+}, 3000);
 
 app.listen(port, () => {
-  return console.log(`server is listening on ${ port }`)
+  return console.log(`server is listening on ${port}`);
 });
---
+++ src/routes/AWSRouter.ts
@@ -0,0 +1,51 @@
+import * as express from 'express';
+import { Response, Request } from 'express';
+import * as passport from 'passport';
+const AWS = require('aws-sdk');
+const uuid = require('uuid');
+
+export class AWSRouter {
+  public router = express.Router();
+  public s3 = new AWS.S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY || 'AKIAVMQNEANEGDQQE3O4',
+    secretAccessKey: process.env.AWS_SECRET_KEY || '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',
+    signatureVersion: 'v4',
+    // region: process.env.AWS_REGION,
+  });
+  constructor() {
+    this.router.get(
+      '/signed_url',
+      // passport.authenticate('jwt', { session: false }),
+      this.preSignURL,
+    );
+  }
+
+  private prepareToPutObject = key =>
+    new Promise((resolve, reject) => {
+      this.s3.getSignedUrl(
+        'putObject',
+        {
+          Bucket: process.env.AWS_BUCKET || 'jewish-light-central',
+          Key: key,
+          ACL: 'public-read',
+        },
+        (err, url) => {
+          if (err) {
+            return reject(err);
+          }
+
+          resolve({ key, url });
+        },
+      );
+    });
+
+  private preSignURL = async (req: Request, res: Response) => {
+    console.log('okkk');
+    // @ts-ignore
+    let key = `images/${uuid()}`;
+    const result = await this.prepareToPutObject(key);
+
+    res.status(200);
+    res.send(result);
+  };
+}
